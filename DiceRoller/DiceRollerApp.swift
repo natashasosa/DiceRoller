a709e9d265246f5f012ff8716ea1058dc35b3a23
natashasosa <natasha.sosa@poatek.com>
2023-10-13 16:25:38 -0300

    First commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Natasha Rebelo on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
